9ac8723414a7be73dbf77fc46fa35b5797d4098a
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-10-09 22:31:53 +0530

    My First VM instance

---
+++ First-VM-Instance/VM_instance.tf
@@ -0,0 +1,22 @@
+provider "google" {
+    credentials = file ("../account.json")
+    project = "terraform-gcp-328318"
+    region = "asia-south1"
+    zone = "asia-south1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "my-first-instance"
+    machine_type = "f1-micro"  
+
+boot_disk {
+    initialize_params {
+        image = "debian-cloud/debian-9"
+    }
+}
+network_interface {
+    network = "default"
+    access_config {
+        }
+}
+}
