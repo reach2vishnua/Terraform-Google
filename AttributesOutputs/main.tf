e2b2b2ff3403a55c6b142b7ca0d2770c559c4ffa
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-10-12 15:19:46 +0530

    Attributes & Output Values Example

--- AttributesOutputs/main.tf
+++ AttributesOutputs/main.tf
@@ -1,20 +1,20 @@
 provider "google" {
-    credentials = file ("../account.json")
-    project = "terraform-gcp-328318"
-    region = "asia-south1"
-    zone = "asia-south1-a"
+  credentials = file("../account.json")
+  project     = "terraform-gcp-328318"
+  region      = "asia-south1"
+  zone        = "asia-south1-a"
 }
 
 resource "google_compute_address" "Static-ip" {
-    name = "my-ipv4-address"  
+  name = "my-ipv4-address"
 }
 
 output "External-IP" {
-    value = google_compute_address.Static-ip.address
+  value = google_compute_address.Static-ip.address
 }
 
 resource "google_storage_bucket" "mybucket" {
-  name          = "my-vishnu-nani"
+  name     = "my-vishnu-nani"
   location = "ASIA-SOUTH1"
 }
 output "Bucket-URL" {
--- First-VM-Instance/VM_instance.tf
+++ First-VM-Instance/VM_instance.tf
@@ -1,22 +1,22 @@
 provider "google" {
-    credentials = file ("../account.json")
-    project = "terraform-gcp-328318"
-    region = "asia-south1"
-    zone = "asia-south1-a"
+  credentials = file("../account.json")
+  project     = "terraform-gcp-328318"
+  region      = "asia-south1"
+  zone        = "asia-south1-a"
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = "my-first-instance"
-    machine_type = "f1-micro"  
+  name         = "my-first-instance"
+  machine_type = "f1-micro"
 
-boot_disk {
+  boot_disk {
     initialize_params {
-        image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-9"
     }
-}
-network_interface {
+  }
+  network_interface {
     network = "default"
     access_config {
-        }
-}
+    }
+  }
 }
